07149b58f14b59112c994c5c5fe95825904df55e
sreebg04 <68550738+sreebg04@users.noreply.github.com>
2021-08-11 18:16:54 +0530

    new test

--- test.py
+++ test.py
@@ -7,32 +7,26 @@ import threading
 from pathlib import Path
 
 
-class Split:
-
-    def __init__(self, source, item):
-        self.source = source
-        self.item = item
-
-    def split(self):
-        linesPerFile = 400000
-        filename = 1
-        result_files = []
-        targetfolder = join(source, Path(str(self.source + "/" + self.item)).stem)
-        if not os.path.exists(targetfolder):
-            os.makedirs(targetfolder)
-        with open(join(self.source, self.item), 'r') as f:
-            csvfile = f.readlines()
-        for i in range(0, len(csvfile), linesPerFile):
-            with open(os.path.join(targetfolder, (str(self.item) + str(filename) + '.csv')),
-                      'w+') as f:
-                if filename > 1:
-                    f.write(csvfile[0])
-                f.writelines(csvfile[i:i + linesPerFile])
-            filename += 1
-        onlyfiles = [f for f in listdir(targetfolder) if isfile(join(targetfolder, f))]
-        for file in onlyfiles:
-            result_files.append(join(targetfolder, file))
-        return result_files
+def split(source, item):
+    linesPerFile = 400000
+    filename = 1
+    result_files = []
+    targetfolder = join(source, Path(str(source + "/" + item)).stem)
+    if not os.path.exists(targetfolder):
+        os.makedirs(targetfolder)
+    with open(join(source, item), 'r') as f:
+        csvfile = f.readlines()
+    for i in range(0, len(csvfile), linesPerFile):
+        with open(os.path.join(targetfolder, (str(item)+str(filename) + '.csv')),
+                  'w+') as f:
+            if filename > 1:
+                f.write(csvfile[0])
+            f.writelines(csvfile[i:i + linesPerFile])
+        filename += 1
+    onlyfiles = [f for f in listdir(targetfolder) if isfile(join(targetfolder, f))]
+    for file in onlyfiles:
+        result_files.append(join(targetfolder, file))
+    return result_files
 
 
 def split_all():
@@ -43,7 +37,7 @@ def split_all():
     print(files)
     print("startupload:  ", datetime.datetime.now())
     for file in files:
-        thread = threading.Thread(target=Split.split, args=(config_datas["source"], file))
+        thread = threading.Thread(target=split, args=(config_datas["source"], file))
         thread_list.append(thread)
     for thr in thread_list:
         thr.start()
