3e0be66d50011f4526c689edacce5d3f78b29328
sreebg04 <68550738+sreebg04@users.noreply.github.com>
2021-08-12 16:14:32 +0530

    new test

--- test2.py
+++ test2.py
@@ -5,29 +5,60 @@ from config import Configure
 import datetime
 import threading
 from pathlib import Path
+import threading
+
+
+# def split_all_files(source, direc, item):
+def split_all_files(source):
+    linesPerFile = 400000
+    filename = 1
+    resultfiles = []
+    # for direc in listdir(source):
+    #     if isdir(join(source, direc)):
+    #         for item in listdir(join(source, direc)):
+    #             print(join(source, direc, item))
+    # datafile = (join(source, direc, item))
+    # newfolder = (join(source, direc, Path(datafile).stem))
+    newfolder = join(os.path.dirname(source), Path(source).stem)
+    if not os.path.exists(newfolder):
+        os.makedirs(newfolder)
+    with open(source, 'r') as f:
+        csvfile = f.readlines()
+    for i in range(0, len(csvfile), linesPerFile):
+        with open((join(newfolder, (str(Path(source).stem) + str(filename)) + '.csv')), 'w+') as f:
+            if filename > 1:
+                f.write(csvfile[0])
+            f.writelines(csvfile[i:i + linesPerFile])
+        filename += 1
+    onlyfiles = [f for f in listdir(newfolder) if isfile(join(newfolder, f))]
+    for file in onlyfiles:
+        resultfiles.append(join(newfolder, file))
+    return resultfiles
+
+
+def split(source):
+    file_list = []
+    thread_list = []
+    for direc in listdir(source):
+        if isdir(join(source, direc)):
+            for item in listdir(join(source, direc)):
+                fileitem = join(source, direc, item)
+                file_list.append(fileitem)
+    print("start", datetime.datetime.now())
+    for file in file_list:
+        thread = threading.Thread(target=split_all_files, args=(file,))
+        thread_list.append(thread)
+    for thr in thread_list:
+        thr.start()
+    for thre in thread_list:
+        thre.join()
+
 
 con = Configure("cred.json")
 config_datas = con.config()
-linesPerFile = 400000
-filename = 1
-resultfiles = []
+split(config_datas["source"])
+print("end", datetime.datetime.now())
 
-for direc in listdir(config_datas["source"]):
-    if isdir(join(config_datas["source"], direc)):
-        for item in listdir(join(config_datas["source"], direc)):
-            datafile = (join(config_datas["source"], direc, item))
-            newfolder = (join(config_datas["source"], direc, Path(datafile).stem))
-            if not os.path.exists(newfolder):
-                os.makedirs(newfolder)
-            with open(datafile, 'r') as f:
-                csvfile = f.readlines()
-            for i in range(0, len(csvfile), linesPerFile):
-                with open((join(newfolder, (str(Path(datafile).stem) + str(filename)) + '.csv')), 'w+') as f:
-                    if filename > 1:
-                        f.write(csvfile[0])
-                    f.writelines(csvfile[i:i + linesPerFile])
-                filename += 1
-            onlyfiles = [f for f in listdir(newfolder) if isfile(join(newfolder, f))]
-            for file in onlyfiles:
-                resultfiles.append(join(newfolder, file))
-
+# con = Configure("cred.json")
+# config_datas = con.config()
+# split_all_files(config_datas["source"])
