b7a6991728eef1e007041310d26edf7d96bae61e
sreebg04 <68550738+sreebg04@users.noreply.github.com>
2021-08-13 15:35:12 +0530

    new test

--- splitfile.py
+++ splitfile.py
@@ -1,33 +1,54 @@
 import os
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
+from config import Configure
+import datetime
+import threading
+from pathlib import Path
+import threading
+import concurrent.futures
 
 
-class Splitfile:
-    linesPerFile = 50000
+def split_all_files(source):
+    linesPerFile = 400000
     filename = 1
-    targetfolder = ""
-    result_files = []
+    resultfiles = []
+    newfolder = join(os.path.dirname(source), Path(source).stem)
+    if not os.path.exists(newfolder):
+        os.makedirs(newfolder)
+    with open(source, 'r') as f:
+        csvfile = f.readlines()
+    for i in range(0, len(csvfile), linesPerFile):
+        with open((join(newfolder, (str(Path(source).stem) + str(filename)) + '.csv')), 'w+') as f:
+            if filename > 1:
+                f.write(csvfile[0])
+            f.writelines(csvfile[i:i + linesPerFile])
+        filename += 1
+    onlyfiles = [f for f in listdir(newfolder) if isfile(join(newfolder, f))]
+    for file in onlyfiles:
+        resultfiles.append(join(newfolder, file))
+    return resultfiles
 
-    def __init__(self, source):
-        self.source = source
 
-    def split(self):
-        Splitfile.targetfolder = join(self.source, "target")
-        if not os.path.exists(Splitfile.targetfolder):
-            os.makedirs(Splitfile.targetfolder)
-        files = [file for file in listdir(self.source) if isfile(join(self.source, file))]
-        for item in files:
-            with open(join(self.source, item), 'r') as f:
-                csvfile = f.readlines()
-            for i in range(0, len(csvfile), Splitfile.linesPerFile):
-                with open(os.path.join(Splitfile.targetfolder, (str(Splitfile.filename) + '.csv')),
-                          'w+') as f:
-                    if Splitfile.filename > 1:
-                        f.write(csvfile[0])
-                    f.writelines(csvfile[i:i + Splitfile.linesPerFile])
-                Splitfile.filename += 1
-            onlyfiles = [f for f in listdir(Splitfile.targetfolder) if isfile(join(Splitfile.targetfolder, f))]
-            for file in onlyfiles:
-                Splitfile.result_files.append(join(Splitfile.targetfolder, file))
-        return Splitfile.result_files
+def split(source):
+    file_list = []
+    result_list = []
+    for direc in listdir(source):
+        if isdir(join(source, direc)):
+            for item in listdir(join(source, direc)):
+                fileitem = join(source, direc, item)
+                file_list.append(fileitem)
+    print("start", datetime.datetime.now())
+
+    for file in file_list:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            future = executor.submit(split_all_files, file)
+            return_value = future.result()
+            result_list.append(return_value)
+    return result_list
+
+
+con = Configure("cred.json")
+config_datas = con.config()
+split(config_datas["source"])
+print("end", datetime.datetime.now())
